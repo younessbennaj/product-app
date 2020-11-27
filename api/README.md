050416983858412badd30c6253cbeb3e89e018f9
Youness BENNAJ <youness.bennaj@gmail.com>
2020-11-27 17:49:16 +0100

    connect to the database server

---
+++ api/README.md
@@ -0,0 +1,21 @@
+# DOCUMENTATION 
+
+# DATABASE 
+
+| Database | Collection | 
+| ----- | ---- | 
+| Shop | Phone |
+
+## Phone document schema (BSON Format)
+
+```
+{ 
+	"_id" : 1,
+	"name" : "AC1 Phone1",
+	"type" : "phone",
+	"price" : 200.05,
+	"rating" : 3.8,
+	"warranty_years" : 1, 
+	"available" : true 
+}
+``` 
--- api/server.js
+++ api/server.js
@@ -1,5 +1,29 @@
 var express = require('express');
+
+const MongoClient = require('mongodb').MongoClient;
+
 var app = express();
+
+//Url de notre Cluster sur mongodb Atlas Cloud
+const uri = "mongodb+srv://dbUser:dbUserPassword@productapp.4nxit.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+//Le nom de notre base de donnée 
+const dbName = "shop";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Connection au serveur de notre base de donnée
+client.connect(err => {
+    if (err) console.log(err);
+    else {
+        console.log('connected');
+        //On récupère la collection "phones" de notre base de donnée "shop"
+        //const collection = client.db(shop).collection("phones");
+
+        //On termine la connection
+        client.close();
+    }
+});
 
 app.get('/', function (req, res) {
     res.send('hello world')
