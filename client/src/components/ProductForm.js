a1fec3865f5cdf0d1fa9ad0d8a5990bf6512e983
Youness BENNAJ <youness.bennaj@gmail.com>
2020-11-28 19:40:55 +0100

    use Formik and Yup lib to build a better form and make some form validation

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react';
 
+import * as Yup from 'yup';
+
+//Formik
+import { Formik, Form, Field } from 'formik';
+
+//Schema du produit pour la validation de formulaire avec Formil et Yup 
+const ProductSchema = Yup.object({
+    //On attend un nom de produit correct
+    name: Yup.string()
+        .min(2, 'The name is too short')
+        .max(50, 'The name is too long')
+        .required('Required'),
+    //Le prix ne doit pas être en dessous de 0 euros
+    price: Yup.number()
+        .min(0, 'The price must be greater than 0')
+        .required('Required'),
+    //La note doit être comprise entre 0 et 5
+    rating: Yup.number()
+        .min(0, 'The rate must be comprised between 0 and 5')
+        .max(5, 'The rate must be comprised between 0 and 5')
+        .required('Required'),
+    warranty_years: Yup.number()
+        .required('Required'),
+    avaible: Yup.boolean()
+        .required('Required')
+});
+
 const ProductForm = ({ mode, productId }) => {
 
     let productModel = {
-        name: "",
-        price: 0,
-        rating: 0,
-        warranty_years: 0,
-        avaible: true
+        name: "test",
+        price: 200.50,
+        rating: 3,
+        warranty_years: 2,
+        avaible: false
     }
 
     const [product, setProduct] = useState(productModel);
@@ -27,43 +54,54 @@ const ProductForm = ({ mode, productId }) => {
     return (
         <div>
             <h1>{mode === "create" ? "Create a new product" : "Update your product"}</h1>
-            <form>
-                <div>
-                    <label htmlFor="name">name</label>
-                    <input onChange={handleChange} type="text" name="name" id="name" value={product.name} />
-                </div>
-                <div>
-                    <label htmlFor="price">price</label>
-                    <input onChange={handleChange} type="number" name="price" id="price" value={product.price} />
-                </div>
-                <div>
-                    <label htmlFor="rating">rating</label>
-                    <select name="rating" id="rating" defaultValue={product.rating}>
-                        {[0, 1, 2, 3, 4, 5].map(rate => {
-                            return <option key={rate} value={rate}>{rate}</option>
-                        })}
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="warranty_years">price</label>
-                    <input type="number" name="warranty_years" id="warranty_years" />
-                </div>
-                <fieldset>
-                    <legend>Select a stock status:</legend>
+            <Formik
+                initialValues={product}
+                validationSchema={ProductSchema}
+                onSubmit={product => {
+                    console.log(product);
+                }}
+            >
+                {({ values }) => (
+                    <Form>
+                        <div>
+                            <label htmlFor="name">name</label>
+                            <Field type="text" name="name" id="name" />
+                        </div>
+                        <div>
+                            <label htmlFor="price">price</label>
+                            <Field type="number" name="price" id="price" />
+                        </div>
+                        <div>
+                            <label htmlFor="rating">rating</label>
+                            <Field as="select" name="rating" id="rating">
+                                {[0, 1, 2, 3, 4, 5].map(rate => {
+                                    return <option key={rate} value={rate}>{rate}</option>
+                                })}
+                            </Field>
+                        </div>
+                        <div>
+                            <label htmlFor="warranty_years">warranty</label>
+                            <Field type="number" name="warranty_years" id="warranty_years" />
+                        </div>
+                        <fieldset>
+                            <legend>Select a stock status:</legend>
 
-                    <div>
-                        <input type="radio" id="avaible" name="stock" value="true" />
-                        <label htmlFor="avaible">avaible</label>
-                    </div>
+                            <div>
+                                <Field type="radio" id="avaible" name="stock" value="true" checked={values.avaible} />
+                                <label htmlFor="avaible">avaible</label>
+                            </div>
 
-                    <div>
-                        <input type="radio" id="out_of_stock" name="stock" value="false" />
-                        <label htmlFor="out_of_stock">out of stock</label>
-                    </div>
+                            <div>
+                                <Field type="radio" id="out_of_stock" name="stock" value="false" checked={!values.avaible} />
+                                <label htmlFor="out_of_stock">out of stock</label>
+                            </div>
 
-                </fieldset>
-                <input type="submit" value={mode === "create" ? "add product" : "update product"} />
-            </form>
+                        </fieldset>
+                        <input type="submit" value={mode === "create" ? "add product" : "update product"} />
+                    </Form>
+                )}
+
+            </ Formik>
         </div>
     );
 }
