0a6c14b5cb711bae8f3ab12b39b7a0e806cc6243
Youness BENNAJ <youness.bennaj@gmail.com>
2020-11-28 12:16:01 +0100

    add GET method on /api/phones/:id to get a specific product

--- api/routes/Phones.js
+++ api/routes/Phones.js
@@ -2,6 +2,9 @@
 var express = require('express')
 var app = express();
 
+//Object Id utils 
+const ObjectId = require('mongodb').ObjectId;
+
 // Méthode pour se connecter à mongoDB et à la base de donnée
 const { connect } = require('../db');
 
@@ -20,5 +23,19 @@ router.get('/', function (req, res) {
         });
     })
 })
+
+//Méthode GET sur la route '/api/phones' : Permet de récupérer le produit de type "phone" avec l'id corresponsant 
+router.get('/:_id', function (req, res) {
+    connect(db => {
+        const collection = db.collection("phones");
+
+        //Comme on laisse mongoDB renseigner le champ _id, il ajouter un Id représenter par un objet ObjectId()
+        //On doit donc utiliser le contructeur ObjectId() pour qu'il nous retourne un objet qui correspond à un Id en base de donnée
+        collection.find({ _id: ObjectId(req.params._id) }).next((err, doc) => {
+            res.send(doc);
+        })
+    })
+})
+
 
 module.exports = router;
